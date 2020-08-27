e7d87ce3d200031873eae51e2871e43781c96757
Jitendra <jitendrasingh.ch11@gmail.com>
2020-08-27 18:13:26 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from "./App";
+
+import App from "../src/Shopping-Cart/App";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
